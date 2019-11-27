06a117ef887bdafce54208c5e263029cd553e6c0
vessemer <vessemer@gmail.com>
2019-11-27 13:14:12 +0300

    update config

--- src/configs/config.py
+++ src/configs/config.py
@@ -28,4 +28,4 @@ API = easydict.EasyDict()
 API.ROOT = 'https://label.cmai.tech'
 API.CASES = API.ROOT + '/api/v1/cases'
 API.KEY = 'jMTCJiJNETMDpwystkl25dFgPbDVpmiSl0Cx6k5pZ7xcUNKu4hbLOpo2UWgIOq8ZBZ7U5Q1djTsyPdmoekNAU3RqhP2kMhp8A5Ef80YDLIchZOGNi77rUrsdlTatwEva'
-API.PORT = 9989
+API.PORT = 8899
