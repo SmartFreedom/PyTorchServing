c8fc0c0ace7171483538396407bc05b5a6ea6df6
vessemer <vessemer@gmail.com>
2020-05-07 20:25:21 +0300

    init configs

--- serving.py
+++ serving.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, jsonify
 from src.configs import config
 from src.api import flask
 
-# set flask params
-
 
 if __name__ == '__main__':
+    config.SHARED.INIT()
     flask.app.run(host="0.0.0.0", debug=config.API.DEBUG, port=config.API.PORT)
--- src/configs/init_config.py
+++ src/configs/init_config.py
@@ -4,13 +4,18 @@ from src.utils import utils
 
 import torch
 import os
+import easydict
 
 
-utils.make_dirs()
+def init():
+    utils.make_dirs()
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = config.CUDA_VISIBLE_DEVICES
-torch.cuda.set_device(config.CUDA_IDX)
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = config.CUDA_VISIBLE_DEVICES
+    torch.cuda.set_device(config.CUDA_IDX)
 
-models = Models()
-preprocess = Preprocess()
+    config.SHARED.models = Models()
+    config.SHARED.preprocess = Preprocess()
+
+PARAMS = easydict.EasyDict()
+PARAMS.INIT = init
