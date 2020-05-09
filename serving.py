7b267eba2dad79d3a61e37634f6e342412dbbffd
vessemer <vessemer@gmail.com>
2020-05-09 14:57:15 +0300

    redis.py

--- serving.py
+++ serving.py
@@ -5,6 +5,6 @@ from src.api import redis
 if __name__ == '__main__':
     config.SHARED.INIT()
     r_api = redis.RedisAPI()
-    r_api.redis_check()
+    r_api.check()
     r_api.listen()
     flask.app.run(host="0.0.0.0", debug=config.API.DEBUG, port=config.API.PORT)
--- src/api/redis.py
+++ src/api/redis.py
@@ -1,29 +1,47 @@
 import redis
 import time
 import traceback
+import imageio
+import json
 
 from src.configs import config
 
 
-def redis_check():
-    try:
-        r = redis.StrictRedis(host=config.API.REDIS.HOST, port=config.API.REDIS.PORT)
-        p = r.pubsub()
-        p.psubscribe(config.API.REDIS.CHANNEL)
-        PAUSE = True
+class RedisAPI:
+    def __init__(self, queue=None):
+        self.queue = queue
+        self.jd = json.JSONDecoder()
+        self.r_connector = redis.StrictRedis(
+            host=config.API.REDIS.HOST,
+            port=config.API.REDIS.PORT,
+            db=config.API.REDIS.DB
+        )
+        self.subscriber = self.r_connector.pubsub()
+        self.subscriber.psubscribe(config.API.REDIS.I_CHANNEL)
 
-        while PAUSE:
-            print("Waiting For redisStarter...")
-            message = p.get_message()
+    def check(self):
+        try:
+            pause = True
+            while pause:
+                print("Waiting For redisStarter...")
+                message = self.subscriber.get_message()
+                if message:
+                    command = message['data']
+                    if command == config.API.REDIS.START:
+                        pause = False
+                time.sleep(1)
+            print("Permission to start...")
+
+        except Exception as e:
+            print("EXCEPTION")
+            print(str(e))
+            print(traceback.format_exc())
+
+    def listen(self):
+        while True:
+            message = self.subscriber.get_message()
             if message:
-                command = message['data']
-                if command == config.API.REDIS.START:
-                    PAUSE = False
+                message = self.jd.decode(message['data'].decode())
+                data = { k: imageio.imread(v) for k, v in message['urls'].items() }
+                print(message)
             time.sleep(1)
-        print("Permission to start...")
-
-    except Exception as e:
-        print("EXCEPTION")
-        print(str(e))
-        print(traceback.format_exc())
-
