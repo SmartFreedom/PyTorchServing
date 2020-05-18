3a04130291b6a41fadf85eefde876138c45e2ad1
vessemer <vessemer@gmail.com>
2020-05-18 17:25:08 +0300

    dataset

--- src/modules/dataset.py
+++ src/modules/dataset.py
@@ -6,7 +6,7 @@ import easydict
 from torch.utils.data import DataLoader, Dataset, IterableDataset
 from torchvision.transforms import ToTensor, Normalize, Compose
 
-from src.api.queue_manager import QueueManager
+from src.api.queue_manager import QueueManager, LQueue
 from src.configs import config
 
 
@@ -16,8 +16,30 @@ img_transform = Compose([
 ])
 
 
-class InferenceDataset(IterableDataset):
-    def __init__(self, queue: QueueManager.LQueue):
+class InferenceDataset(Dataset):
+    def __init__(self, queue: LQueue):
+        self.queue = queue
+
+    def __getitem__(self, idx):
+        return self.process(self.queue[idx])
+
+    def process(self, data):
+        data = {
+            "image": data['image'],
+            "pid": data['channel'],
+            "side": data['side'],
+            "shape": data['image'].shape
+        }
+        data = easydict.EasyDict(data)
+        data.image = img_transform(np.expand_dims(data.image, -1))
+        return data
+
+    def __len__(self):
+        return len(self.queue)
+
+
+class InferenceIterableDataset(IterableDataset):
+    def __init__(self, queue: LQueue):
         self.queue = queue
 
     def __iter__(self):
@@ -25,9 +47,13 @@ class InferenceDataset(IterableDataset):
 
     def process(self):
         data = self.queue.pop()
+        if data is None:
+            raise StopIteration
+
         data = {
             "image": data['image'],
             "pid": data['channel'],
+            "side": data['side'],
             "shape": data['image'].shape
         }
         data = easydict.EasyDict(data)
@@ -36,7 +62,7 @@ class InferenceDataset(IterableDataset):
 
 
 class DatasetIterator:
-    def __init__(self, dataset: InferenceDataset):
+    def __init__(self, dataset: InferenceIterableDataset):
         self.dataset = dataset
 
     def __next__(self):
