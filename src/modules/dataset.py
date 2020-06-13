dd1f97fee0c25f4fbebf87514c1680aa1baf3db8
vessemer <vessemer@gmail.com>
2020-06-13 19:21:49 +0300

    dataset update

--- src/modules/dataset.py
+++ src/modules/dataset.py
@@ -3,21 +3,58 @@ import cv2
 import numpy as np
 import easydict
 
+import torch
 from torch.utils.data import DataLoader, Dataset, IterableDataset
 from torchvision.transforms import ToTensor, Normalize, Compose
 
-from src.api.queue_manager import QueueManager, LQueue
 from src.configs import config
+import src.modules.augmentations as augs
 
 
-img_transform = Compose([
-    ToTensor(),
-    Normalize(mean=config.MEAN, std=config.STD)
-])
+def inference_collater(data):
+    pids = [ pid for s in data for pid in [s['pid']] * 8 ]
+    sides = [ side for s in data for side in [s['side']] * 8 ]
+    shapes = [ pid for s in data for pid in [s['shape']] * 8 ]
+    imgs = [ s['image'] for s in data ]
+    _shapes = np.array([ s.shape[1:] for s in imgs ])
+    max_shape = _shapes.max() 
+    max_shape += (128 - max_shape % 128) * (max_shape % 128 != 0)
+
+    imgs = [np.pad(
+        s, 
+        ((0, 0),
+        (0, max_shape - s.shape[1]),
+        (0, max_shape - s.shape[2])),
+        mode='constant',
+        constant_values=np.median(s)
+    ) for s in imgs ]
+    imgs = torch.stack([ 
+        torch.Tensor(im.copy())
+        for s in imgs for im in augs._rotate_mirror_do(s) ])
+    
+    return { 'image': imgs, 'pid': pids, 'shape': shapes, 'side': sides }
+
+
+def infer_on_batch(model, data):
+    batch = [data['image']]
+    if np.array(data['shape']).max() > 1000:
+        batch = [ torch.unsqueeze(d, 0) for d in data['image'] ]
+
+    torch.cuda.empty_cache()
+    predict = list()
+    for image in batch:
+        image = torch.autograd.Variable(image).cuda()
+        predict.append(model(image).sigmoid().data.cpu().numpy())
+        image = image.data.cpu().numpy()
+        torch.cuda.empty_cache()
+    predict = np.squeeze(np.stack(predict))
+    return predict if len(predict.shape) == 4 else np.squeeze(predict, axis=0)
 
 
 class InferenceDataset(Dataset):
-    def __init__(self, queue: LQueue):
+    def __init__(self, queue, transform):
+        """queue: qm.LQueue"""
+        self.img_transform = transform
         self.queue = queue
 
     def __getitem__(self, idx):
@@ -28,10 +65,12 @@ class InferenceDataset(Dataset):
             "image": data['image'],
             "pid": data['channel'],
             "side": data['side'],
+            "mask": data['mask'][np.newaxis] if 'mask' in data.keys() else np.nan,
+            "scale_factor": data['scale_factor'] if 'scale_factor' in data.keys() else np.nan,
             "shape": data['image'].shape
         }
         data = easydict.EasyDict(data)
-        data.image = img_transform(np.expand_dims(data.image, -1))
+        data.image = self.img_transform(np.expand_dims(data.image, -1))
         return data
 
     def __len__(self):
@@ -39,8 +78,13 @@ class InferenceDataset(Dataset):
 
 
 class InferenceIterableDataset(IterableDataset):
-    def __init__(self, queue: LQueue):
+    def __init__(self, queue):
+        """queue: qm.LQueue"""
         self.queue = queue
+        self.img_transform = Compose([
+            ToTensor(),
+            Normalize(mean=config.PROCESS.RoI.MEAN, std=config.PROCESS.RoI.STD)
+        ])
 
     def __iter__(self):
         return DatasetIterator(self)
@@ -57,7 +101,7 @@ class InferenceIterableDataset(IterableDataset):
             "shape": data['image'].shape
         }
         data = easydict.EasyDict(data)
-        data.image = img_transform(np.expand_dims(data.image, -1))
+        data.image = self.img_transform(np.expand_dims(data.image, -1))
         return data
 
 
