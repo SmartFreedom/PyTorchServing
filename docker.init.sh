1b2dc6e53f44d8353f7d7a512b56d2ca4fb0ee2f
vessemer <vessemer@gmail.com>
2020-09-04 05:13:20 +0300

    init sh

--- docker.init.sh
+++ docker.init.sh
@@ -14,6 +14,7 @@ if [ -z "${i}" ]; then
     usage
 fi
 
+echo "CUDA visible device set to ... $i"
 export TORCH_SERVER_V=0.4
 nvidia-docker image build -t torch-server:$TORCH_SERVER_V .
-nvidia-docker run --rm -it -p 0.0.0.0:9767:22/tcp -p 0.0.0.0:9769:9769/tcp --name torchy  torch-server:$TORCH_SERVER_V --runtime=nvidia --ipc=host -e NVIDIA_VISIBLE_DEVICES=$i -e NVIDIA_DRIVER_CAPABILITIES=compute,utility
+NV_GPU=$i nvidia-docker run --rm -it -p 0.0.0.0:9767:22/tcp -p 0.0.0.0:9769:9769/tcp --name torchy  torch-server:$TORCH_SERVER_V --runtime=nvidia --ipc=host -e NVIDIA_VISIBLE_DEVICES=$i -e NVIDIA_DRIVER_CAPABILITIES=compute,utility
