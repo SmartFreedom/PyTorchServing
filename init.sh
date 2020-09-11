32f4eaf745586f3ea0a9b55e82b16e6c228fec8e
vessemer <vessemer@gmail.com>
2020-09-11 17:25:12 +0300

    init direct

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#nohup redis-server --port 9358 > $HOME/redis.log 2>&1 &
+# nohup redis-server --port 9358 > $HOME/redis.log 2>&1 &
 # to renew the prompt & init conda setup
+$HOME/conda/envs/py36/envs/crt/bin/python serving.py > $HOME/torch.log 2>&1 &i
+# conda activate crt
+# python serving.py > $HOME/torch.log 2>&1 &
 /bin/bash
-conda activate crt
-python serving.py > $HOME/torch.log 2>&1 &
 
